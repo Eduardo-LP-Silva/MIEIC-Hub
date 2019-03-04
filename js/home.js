07158d0735482274b3fbaeed5bd725af4dce9d1a
Eduardo Silva <eduardo__lps@hotmail.com>
2019-03-04 16:16:47 +0000

    Added cart to navbar

---
+++ js/home.js
@@ -0,0 +1,19 @@
+window.onload = function()
+{
+    setAspectRatio();
+}
+
+window.onresize = function()
+{
+    setAspectRatio();
+}
+
+function setAspectRatio()
+{
+    let cols = document.querySelectorAll(".col");
+
+    for(let i = 0; i < cols.length; i++)
+    {
+        cols[i].style.height = "" + cols[i].clientWidth + "px";
+    }
+}
