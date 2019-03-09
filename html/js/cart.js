6b36ce94bef3e9cf28819d4ea28758269559f7cb
Eduardo Silva <eduardo__lps@hotmail.com>
2019-03-09 22:57:59 +0000

    Added cart page

---
+++ html/js/cart.js
@@ -0,0 +1,31 @@
+window.onload = function()
+{
+    addRemoveListeners();
+    updateFinalPrice();
+}
+
+function addRemoveListeners()
+{
+    let removeBtns = document.querySelectorAll(".list-item > i");
+    let list = document.querySelector("#list");
+
+    for(let i = 0; i < removeBtns.length; i++)
+        removeBtns[i].addEventListener("click", function(event)
+        {
+            event.preventDefault();
+            list.removeChild(removeBtns[i].parentNode);
+            updateFinalPrice();
+        });
+}
+
+function updateFinalPrice()
+{
+    let finalPrice = 0, items = document.querySelectorAll(".list-item");
+
+    for(let i = 0; i < items.length; i++)
+        finalPrice += parseFloat(items[i].querySelector("span:nth-child(5)").textContent);
+
+    let finalPriceElement = document.querySelector("#list > div:last-child > span");
+
+    finalPriceElement.textContent = "Total: " + finalPrice;
+}
