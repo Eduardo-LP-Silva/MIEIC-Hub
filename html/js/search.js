a3cc9e709abdb10d788f9b5b9e9da3e2e3d17b87
Eduardo Silva <eduardo__lps@hotmail.com>
2019-03-12 00:25:29 +0000

    Changed search

--- html/js/search.js
+++ html/js/search.js
@@ -1,6 +1,7 @@
 window.onload = function()
 {
     setAspectRatio();
+    setFilterListener();
 }
 
 function setAspectRatio()
@@ -11,4 +12,20 @@ function setAspectRatio()
     {
         cols[i].style.height = "" + cols[i].clientWidth + "px";
     }
+}
+
+function setFilterListener()
+{
+    let dropdownItems = document.querySelectorAll("#search_parameters .dropdown-item");
+
+    for(let i = 0; i < dropdownItems.length; i++)
+        dropdownItems[i].addEventListener("click", function(event)
+        {
+            event.preventDefault();
+            
+            let filter = document.querySelector("#search_parameters > .dropdown > span");
+
+            filter.textContent = dropdownItems[i].textContent;
+
+        });
 }
