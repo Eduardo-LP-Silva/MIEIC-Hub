eee11cba5ec4f5011771365e8ce0fed2f9414450
joanasmramos <joanasmramos98@gmail.com>
2019-04-23 22:07:45 +0100

    added date in register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,43 +1,3 @@
-{{-- @extends('layouts.app')
-
-@section('content')
-<form method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
-
-    <label for="name">Name</label>
-    <input id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
-    @if ($errors->has('name'))
-      <span class="error">
-          {{ $errors->first('name') }}
-      </span>
-    @endif
-
-    <label for="email">E-Mail Address</label>
-    <input id="email" type="email" name="email" value="{{ old('email') }}" required>
-    @if ($errors->has('email'))
-      <span class="error">
-          {{ $errors->first('email') }}
-      </span>
-    @endif
-
-    <label for="password">Password</label>
-    <input id="password" type="password" name="password" required>
-    @if ($errors->has('password'))
-      <span class="error">
-          {{ $errors->first('password') }}
-      </span>
-    @endif
-
-    <label for="password-confirm">Confirm Password</label>
-    <input id="password-confirm" type="password" name="password_confirmation" required>
-
-    <button type="submit">
-      Register
-    </button>
-    <a class="button button-outline" href="{{ route('login') }}">Login</a>
-</form>
-@endsection --}}
-
 @extends('layouts.auth')
 
 @section('stylesheets')
@@ -78,9 +38,12 @@
 
       <input id="password-confirm" type="password" name="password_confirmation" placeholder="🔐   Confirm Password" required>
      
-      {{-- <form class="dateform">
-          <a>Birthday:</a> <input type="date" name="bithdaydate">
-      </form> --}}
+      <a>Birthday:</a> <input class="dateform" id="birth_date" name="birth_date" type="date" name="bithdaydate">
+      @if ($errors->has('birth_date'))
+        <span class="error">
+            {{ $errors->first('birth_date') }}
+        </span>
+      @endif
 
       <a><input type="submit" value="Register"></a> 
       <a class="button button-outline" href="{{ route('login') }}">Login instead</a>
